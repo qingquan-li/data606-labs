3334bb54456d4ca03c535159e59096312a0ce6d5
Qingquan Li <liqqisme@gmail.com>
2026-02-08 18:48:46 -0500

    Initial a data606-labs project and checkout Lab1

---
+++ get-started.R
@@ -0,0 +1,19 @@
+# Install remotes package
+# install.packages("remotes")
+
+# Install DATA606 from GitHub
+# remotes::install_github('jbryer/DATA606')
+
+# Lists data available in the package
+# data(package="DATA606")
+
+# Start Lab 1 (copies files into your working directory)
+# DATA606::startLab('Lab1')
+
+# Markdown will first be converted to LaTeX before creating the PDF file.
+# As such, a LaTeX converter is necessary.
+# install.packages('tinytex')
+# tinytex::install_tinytex()
+
+# Show installed R packages
+# installed.packages()[, c("Package", "Version")]
